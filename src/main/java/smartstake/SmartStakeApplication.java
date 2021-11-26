1160124c13aac306e7ef7366babfe8e0585f6d96
Priscilla Odame <priscillaodame5@gmail.com>
2021-11-26 16:41:23 +0000

    connect to database

--- src/main/java/smartstake/SmartStakeApplication.java
+++ src/main/java/smartstake/SmartStakeApplication.java
@@ -3,11 +3,13 @@ package smartstake;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SmartStakeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmartStakeApplication.class, args);
 	}
+
 
 }
