ccab06fda97036ff8b201dfb7a7064a521866f2f
Nwokafor Chukwuebuka <nwokafor.choongsaeng@turntabl.io>
2021-11-29 15:50:29 +0000

    added market-data route and basic implementation

--- src/main/java/smartstake/controllers/MarketData.java
+++ src/main/java/smartstake/controllers/MarketData.java
@@ -1,4 +1,14 @@
 package smartstake.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/market-data")
 public class MarketData {
+    @GetMapping
+    public String getMarketData() {
+        return "market data";
+    }
 }
