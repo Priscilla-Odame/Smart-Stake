bfe4d8583787822e6ac49fb15bc9803a78f71fda
Priscilla Odame <priscillaodame5@gmail.com>
2021-11-30 15:57:02 +0000

    implement post method

--- src/main/java/smartstake/controllers/PortfolioController.java
+++ src/main/java/smartstake/controllers/PortfolioController.java
@@ -1,9 +1,7 @@
 package smartstake.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import smartstake.entities.Portfolio;
 import smartstake.services.PortfolioService;
 import java.util.List;
@@ -22,5 +20,11 @@ public class PortfolioController {
     @RequestMapping(value = "/portfolio/{id}")
     public Portfolio getPortfolios(@PathVariable Long id) {
         return portfolioService.getPortfolio(id);
+    }
+
+    //Create a new portfolio
+    @RequestMapping(value="/portfolios", method= RequestMethod.POST)
+    public void addPortfolio(@RequestBody Portfolio portfolio) {
+        portfolioService.addPortfolio(portfolio);
     }
 }
--- src/main/java/smartstake/services/PortfolioService.java
+++ src/main/java/smartstake/services/PortfolioService.java
@@ -5,33 +5,46 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import smartstake.entities.Client;
 import smartstake.entities.Portfolio;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class PortfolioService {
 
+    //List all portfolios
+    Portfolio portfolio1 = new Portfolio(
+            1L,
+            new Client(1,"Priscilla","Odame","1234","admin@gmail.com"),
+            "stock",
+            "50% shares from MSOFT and 20% from AAPL"
+    );
+    Portfolio portfolio2 = new Portfolio(
+            2L,
+            new Client(1,"Jeff","Asmah","1234","admin@gmail.com"),
+            "stock",
+            "20% shares from AAPL and 20% from MSOFT"
+    );
+
+    private List<Portfolio> portfolios = new ArrayList<>(Arrays.asList(portfolio1, portfolio2));
+
     public List<Portfolio> getAllPortfolios() {
-        Portfolio portfolio1 = new Portfolio(
-                1L,
-                new Client(1,"Priscilla","Odame","1234","admin@gmail.com")
-        );
-        Portfolio portfolio2 = new Portfolio(
-                2L,
-                new Client(1,"Jeff","Asmah","1234","admin@gmail.com")
-        );
-
-        return Arrays.asList(portfolio1, portfolio2);
-
+        return portfolios;
     }
 
+    //Get a portfolio by id
     public Portfolio getPortfolio(Long id) {
-        Portfolio portfolio = getAllPortfolios().stream()
+        Portfolio portfolio = portfolios.stream()
                 .filter(t -> id.equals(t.getId()))
                 .findFirst()
                 .orElse(null);
 
         return portfolio;
+    }
+
+    //Create a portfolio
+    public void addPortfolio(Portfolio portfolio) {
+        portfolios.add(portfolio);
     }
 //    public void addLocation(Portfolio portfolio) {
 //        portfolios.add(portfolio);
