65ca2d33ca62f61a8b016e4ffb3557f27e0c1d1f
Priscilla Odame <priscillaodame5@gmail.com>
2021-11-30 14:44:28 +0000

    Add another constructor to portfolio.java

--- src/main/java/smartstake/entities/Portfolio.java
+++ src/main/java/smartstake/entities/Portfolio.java
@@ -16,6 +16,23 @@ public class Portfolio {
     public Portfolio() {
     }
 
+    public Portfolio(Long id, Client client) {
+        this.id = id;
+        this.client = client;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
     public Long getId() {
         return id;
     }
