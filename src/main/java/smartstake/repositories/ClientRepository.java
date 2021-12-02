52493b1779f735174b48d9efd9c7ecf2c0f3da8d
someone <someone@someplace.com>
2021-12-02 08:23:41 +0000

    resolved conflicts

--- src/main/java/smartstake/repositories/ClientRepository.java
+++ src/main/java/smartstake/repositories/ClientRepository.java
@@ -3,7 +3,6 @@ package smartstake.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import smartstake.entities.Client;
 
-
 public interface ClientRepository extends JpaRepository<Client, Integer> {
 
 }
